bdd648fe69c4ef235893c1106742d3f3764002bb
Manu <manuelguese01@gmail.com>
2025-10-15 14:35:37 +0200

    Add Experience Section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://Drooliito.github.io/Portfolio-Github',
   base: '/Portfolio-Github/',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
