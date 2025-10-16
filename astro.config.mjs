cccd974b81bd469a89673c181afd2927633279b3
Manu <manuelguese01@gmail.com>
2025-10-16 12:56:30 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+const isVercel = process.env.VERCEL === "1";
 
 export default defineConfig({
-  site: 'https://drooliito.github.io/Portfolio-Github',
-  base: '/Portfolio-Github/',
-  output: 'static',
+  site: isVercel
+    ? "https://tusitio.vercel.app"
+    : "https://drooliito.github.io/Portfolio-Github",
+  base: isVercel ? "/" : "/Portfolio-Github/",
+  output: "static",
 });
