987bc18f850dae0af8c8da64b9c988063d000e31
Manu <manuelguese01@gmail.com>
2025-10-16 12:53:18 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Drooliito.github.io/Portfolio-Github',
+  site: 'https://drooliito.github.io/Portfolio-Github',
   base: '/Portfolio-Github/',
+  output: 'static',
 });
